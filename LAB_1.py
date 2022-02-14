62c3e679e6a5b1eaa70d55e3ec260c6b09f71ed8
SadPlayYA <tvoy_nosok@mail.ru>
2022-02-15 02:10:54 +0300

    Complete

---
+++ LAB_1.py
@@ -0,0 +1,19 @@
+import math
+print('Введите коэффиценты квадратного уравнения вида:')
+print('ax^2+bx+c=0')
+a = float(input('Введите коэффицент а = '))
+b = float(input('Введите коэффицент b = '))
+c = float(input('Введите коэффицент c = '))
+d = b * b - 4 * a * c
+x1 = 0
+x2 = 0                                             
+x = 0
+print('Дискриминант = ', d)
+print('Ответ: ')
+if d > 0:
+    x1 = (-b + math.sqrt(d)) / 2 * a ; print('x1 = ', x1)
+    x2 = (-b - math.sqrt(d)) / 2 * a ; print('x2 = ', x2)
+elif d == 0:
+    x = -b / 2 * a ; print('x = ', x)
+else:
+    print("Ваше уравнение не имеет корней")
